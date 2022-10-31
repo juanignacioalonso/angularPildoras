27cde7f5ee83ea9fb2c62b2c17be5fa6657480cd
Juan Ignacio <zalbak87@gmail.com>
2022-10-31 12:13:45 -0300

    Routing II

--- app-empleados/src/app/app.component.ts
+++ app-empleados/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { Empleado } from './empleado.model';
-import { EmpleadosService } from './empleados.service';
-import { ServicioEmpleadosService } from './servicio-empleados.service';
+
 
 @Component({
   selector: 'app-root',
@@ -9,21 +7,12 @@ import { ServicioEmpleadosService } from './servicio-empleados.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  titulo = 'Listado de Empleados';
+  
 
-  constructor(private miServicio:ServicioEmpleadosService, private empleadosService:EmpleadosService){
-    this.empleados=this.empleadosService.empleados;
+  constructor(){
+    
   }
 
-  empleados:Empleado[]=[];
-  agregarEmpleado(){
-    let miEmpleado=new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
-    //this.miServicio.muestraMensaje("Nombre del empleado: "+miEmpleado.nombre+" "+miEmpleado.apellido);
-    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
-  }
-  cuadroNombre:string="";
-  cuadroApellido:string="";
-  cuadroCargo:string="";
-  cuadroSalario:number=0;
+  
 
 }
